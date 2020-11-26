c8645f06a9057bac7e8f60763fb5b5a360f11536
Gabriel Maire <gabrielmaire69@gmail.com>
2020-11-26 15:49:58 +0100

    avancement sur le profil

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,9 @@ class ProfilesController < ApplicationController
     @accepted_requests = current_user.rental_requests.where(status: "acceptée")
     @refused_requests = current_user.rental_requests.where(status: "refusée")
 
+    # @clavier_instruments = current_user.instrument.where(status: "Clavier")
+    # @vent_instruments = current_user.instrument.where(status: "Instrument à vent")
+
     @users = current_user
   end
 end
