42e1ed916aa551f0f36cf643f4bc36f1e8225708
Gabriel Maire <gabrielmaire69@gmail.com>
2020-11-26 18:29:08 +0100

    FInilisation profile + controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,14 @@
 class ProfilesController < ApplicationController
   def show
-    @pending_requests = current_user.rental_requests.where(status: "en attente")
+    @pending_requests  = current_user.rental_requests.where(status: "en attente")
     @accepted_requests = current_user.rental_requests.where(status: "acceptée")
-    @refused_requests = current_user.rental_requests.where(status: "refusée")
+    @refused_requests  = current_user.rental_requests.where(status: "refusée")
+
+    @my_requests = RentalRequest.joins(:instrument).where(instruments: { owner: current_user })
+    # @my_pending_requests = @my_requests.where(status: "en attente")
+
+    # @my_accepted_requests = current_user.instruments.rental_requests.where(status: "acceptée")
+    # @my_refused_requests  = current_user.instruments.rental_requests.where(status: "refusée")
 
     # @clavier_instruments = current_user.instrument.where(status: "Clavier")
     # @vent_instruments = current_user.instrument.where(status: "Instrument à vent")
